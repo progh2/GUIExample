fc004625422305d0c562bfd46cb8b718d1a1de64
Gihun Ham <ham@e-mirim.hs.kr>
2022-09-14 10:08:10 +0900

    레이블, 링크레이블, 체크박스 UI 실습

--- GUIExample/Form1.cs
+++ GUIExample/Form1.cs
@@ -15,7 +15,68 @@ namespace GUIExample
         public Form1()
         {
             InitializeComponent();
+
+            Label label = new Label()
+            {
+                Text = "글자",
+                Location = new Point(10, 110)
+            };
+            Controls.Add(label);
+
+            LinkLabel linkLabel = new LinkLabel()
+            {
+                Text = "Naver",
+                Location = new Point(10, 150)
+            };
+            linkLabel.Click += LabelClick;
+            Controls.Add(linkLabel);
+
+            CheckBox checkBox1 = new CheckBox();
+            CheckBox checkBox2 = new CheckBox();
+            CheckBox checkBox3 = new CheckBox();
+            Button button = new Button();
+
+            checkBox1.Text = "감자";
+            checkBox2.Text = "고구마";
+            checkBox3.Text = "토마토";
+            button.Text = "클릭";
+
+            checkBox1.Location = new Point(140, 110);
+            checkBox2.Location = new Point(140, 140);
+            checkBox3.Location = new Point(140, 170);
+            button.Location = new Point(140, 200);
+
+            button.Click += ButtonClick;
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
         }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            foreach(var item in Controls)
+            {
+                if(item is CheckBox)
+                {
+                    CheckBox checkBox = (CheckBox)item;
+                    if (checkBox.Checked)
+                    {
+                        list.Add(checkBox.Text);
+                    }
+                }
+            }
+            MessageBox.Show(string.Join(", ", list));
+        }
+
+        private void LabelClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+            //System.Diagnostics.Process.Start("https://naver.com");
+        }
+
         int STEP = 20;
         private void 상태증가ToolStripMenuItem_Click(object sender, EventArgs e)
         {
